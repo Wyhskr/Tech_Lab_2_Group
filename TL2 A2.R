b240dc2c91dcfa2205929f285645a47be1db1aec
Matt <13219670@student.uts.edu.au>
2018-09-26 16:51:32 +1000

    V2 Complete

--- TL2 A2.R
+++ TL2 A2.R
@@ -4,13 +4,24 @@ library(dplyr)
 
 setwd("~/OneDrive - University of Technology Sydney/2018/Semester 2/Tech Lab 2/Assessments/Ass2/TL2: A2")
 
-countries <- read_csv("MJ.csv")
-state <- read_csv("RW.csv")
-state2 <- read_csv("RW2.csv")
-state3 <- read_csv("RW3.csv")
+countries <- read_csv("Countries.csv")
+state <- read_csv("States.csv")
+ind <- read_csv("Ind.csv")
 
-fullJoin <- countries %>% 
-  full_join(state2, by="Percent") %>%
-  full_join(countries, by="Percent") %>% 
-  select(Country, State, Percent)
+leftJoin <- countries %>% 
+  left_join(state, by="Country") %>%
+  left_join(ind, by=c("State","Country")) %>% 
+  select(Country, State, Code, Region, Income,'National Percent', 'State Percent','Ind Percent')
+
+
+
+
+
+
+
+
+
+
+
+
 
